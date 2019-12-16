529235e6d97ab345190f6a00cd4abe66acc60278
Aric Stewart <aric@codeweavers.com>
2019-12-16 07:51:57 -0600

    Beginning of RESTful api implementation using Slim

---
+++ api/src/Middleware/CiabMiddleware.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+/*.
+    require_module 'standard';
+.*/
+
+namespace App\Middleware;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+require_once __DIR__."/../../../functions/session.inc";
+
+class CiabMiddleware
+{
+
+
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        $user = $request->getAttribute('oauth2-token')['user_id'];
+        if (!empty($user)) {
+            \loadAccount($user);
+        }
+        $response = $next($request, $response);
+        return $response;
+
+    }
+
+
+    /* End CiabMiddleware */
+}
